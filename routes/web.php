6b42d32893e3fe312fab5fa011f8a999abac274c
Angga Saputra <angsa0077@gmail.com>
2023-11-29 04:59:59 +0700

    membuat report profits

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Apps\RoleController;
+use App\Http\Controllers\Apps\SaleController;
+use App\Http\Controllers\Apps\UserController;
+use App\Http\Controllers\Apps\ProfitController;
+use App\Http\Controllers\Apps\ProductController;
+use App\Http\Controllers\Apps\CategoryController;
+use App\Http\Controllers\Apps\CustomerController;
+use App\Http\Controllers\Apps\DashboardController;
+use App\Http\Controllers\Apps\PermissionController;
+use App\Http\Controllers\Apps\TransactionController;
 
 // route home
 Route::get('/', function () {
@@ -14,60 +24,72 @@ Route::prefix('apps')->group(function() {
     Route::group(['middleware' => ['auth']], function(){
 
         // route dashboard
-        Route::get('dashboard', App\Http\Controllers\Apps\DashboardController::class)->name('apps.dashboard');
+        Route::get('dashboard', DashboardController::class)->name('apps.dashboard');
 
         // route permissions
-        Route::get('/permissions', \App\Http\Controllers\Apps\PermissionController::class)->name('apps.permissions.index')
+        Route::get('/permissions', PermissionController::class)->name('apps.permissions.index')
         ->middleware('permission:permissions.index');
 
         // route resource roles
-        Route::resource('/roles', \App\Http\Controllers\Apps\RoleController::class, ['as' => 'apps'])
+        Route::resource('/roles', RoleController::class, ['as' => 'apps'])
         ->middleware('permission:roles.index|roles.create|roles.edit|roles.delete');
 
         // route resource users
-        Route::resource('/users', \App\Http\Controllers\Apps\UserController::class, ['as' => 'apps'])
+        Route::resource('/users', UserController::class, ['as' => 'apps'])
         ->middleware('permission:users.index|users.create|users.edit|users.delete');
 
         //route resource categories
-        Route::resource('/categories', \App\Http\Controllers\Apps\CategoryController::class, ['as' => 'apps'])
+        Route::resource('/categories', CategoryController::class, ['as' => 'apps'])
         ->middleware('permission:categories.index|categories.create|categories.edit|categories.delete');    
 
         //route resource products
-        Route::resource('/products', \App\Http\Controllers\Apps\ProductController::class, ['as' => 'apps'])
+        Route::resource('/products', ProductController::class, ['as' => 'apps'])
         ->middleware('permission:products.index|products.create|products.edit|products.delete');
 
         //route resource customers
-        Route::resource('/customers', \App\Http\Controllers\Apps\CustomerController::class, ['as' => 'apps'])
+        Route::resource('/customers', CustomerController::class, ['as' => 'apps'])
             ->middleware('permission:customers.index|customers.create|customers.edit|customers.delete');
 
         //route transaction
-        Route::get('/transactions', [\App\Http\Controllers\Apps\TransactionController::class, 'index'])->name('apps.transactions.index');
+        Route::get('/transactions', [TransactionController::class, 'index'])->name('apps.transactions.index');
 
         //route transaction searchProduct
-        Route::post('/transactions/searchProduct', [\App\Http\Controllers\Apps\TransactionController::class, 'searchProduct'])->name('apps.transactions.searchProduct');
+        Route::post('/transactions/searchProduct', [TransactionController::class, 'searchProduct'])->name('apps.transactions.searchProduct');
         
         //route transaction addToCart
-        Route::post('/transactions/addToCart', [\App\Http\Controllers\Apps\TransactionController::class, 'addToCart'])->name('apps.transactions.addToCart');
+        Route::post('/transactions/addToCart', [TransactionController::class, 'addToCart'])->name('apps.transactions.addToCart');
 
         //route transaction destroyCart
-        Route::post('/transactions/destroyCart', [\App\Http\Controllers\Apps\TransactionController::class, 'destroyCart'])->name('apps.transactions.destroyCart');
+        Route::post('/transactions/destroyCart', [TransactionController::class, 'destroyCart'])->name('apps.transactions.destroyCart');
 
         //route transaction store
-        Route::post('/transactions/store', [\App\Http\Controllers\Apps\TransactionController::class, 'store'])->name('apps.transactions.store');
+        Route::post('/transactions/store', [TransactionController::class, 'store'])->name('apps.transactions.store');
 
         //route transaction print
-        Route::get('/transactions/print', [\App\Http\Controllers\Apps\TransactionController::class, 'print'])->name('apps.transactions.print');
+        Route::get('/transactions/print', [TransactionController::class, 'print'])->name('apps.transactions.print');
 
         //route sales index
-        Route::get('/sales', [\App\Http\Controllers\Apps\SaleController::class, 'index'])->middleware('permission:sales.index')->name('apps.sales.index');
+        Route::get('/sales', [SaleController::class, 'index'])->middleware('permission:sales.index')->name('apps.sales.index');
         
         //route sales filter
-        Route::get('/sales/filter', [\App\Http\Controllers\Apps\SaleController::class, 'filter'])->name('apps.sales.filter');
+        Route::get('/sales/filter', [SaleController::class, 'filter'])->name('apps.sales.filter');
 
         //route sales export excel
-        Route::get('/sales/export', [\App\Http\Controllers\Apps\SaleController::class, 'export'])->name('apps.sales.export');
+        Route::get('/sales/export', [SaleController::class, 'export'])->name('apps.sales.export');
 
         //route sales print pdf
-        Route::get('/sales/pdf', [\App\Http\Controllers\Apps\SaleController::class, 'pdf'])->name('apps.sales.pdf');
+        Route::get('/sales/pdf', [SaleController::class, 'pdf'])->name('apps.sales.pdf');
+
+        //route profits index
+        Route::get('/profits', [ProfitController::class, 'index'])->middleware('permission:profits.index')->name('apps.profits.index');
+
+        //route profits filter
+        Route::get('/profits/filter', [ProfitController::class, 'filter'])->name('apps.profits.filter');
+
+        //route profits export
+        Route::get('/profits/export', [ProfitController::class, 'export'])->name('apps.profits.export');
+
+        //route profits pdf
+        Route::get('/profits/pdf', [ProfitController::class, 'pdf'])->name('apps.profits.pdf');
     });
 });
